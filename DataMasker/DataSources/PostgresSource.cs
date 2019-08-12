d1c8ab3ee9c8b45c01f503644b6a13376090af86
idir\sookeke <idir\sookeke@4b9da133-2f76-0b4d-b56b-605931d648fd>
2019-08-12 02:56:13 +0000

    change in string to object
    git-svn-id: https://svn.th.gov.bc.ca/DA/trunk/Data%20Integration/DataMasker%20IMB/DataMasker/trunk@394 4b9da133-2f76-0b4d-b56b-605931d648fd

--- DataMasker/DataSources/PostgresSource.cs
+++ DataMasker/DataSources/PostgresSource.cs
@@ -248,7 +248,49 @@ namespace DataMasker.DataSources
 
         public DataTable SpreadSheetTable(IEnumerable<IDictionary<string, object>> parents)
         {
-            throw new NotImplementedException();
+            var table = new DataTable();
+
+            foreach (var parent in parents)
+            {
+                var children = parent.Values
+                                     .OfType<IEnumerable<IDictionary<string, object>>>()
+                                     .ToArray();
+
+                var length = children.Any() ? children.Length : 1;
+
+                var parentEntries = parent.Where(x => x.Value is object)
+                                          .Repeat(length)
+                                          .ToLookup(x => x.Key, x => x.Value);
+                var childEntries = children.SelectMany(x => x.First())
+                                           .ToLookup(x => x.Key, x => x.Value);
+
+                var allEntries = parentEntries.Concat(childEntries)
+                                              .ToDictionary(x => x.Key, x => x.ToArray());
+
+                var headers = allEntries.Select(x => x.Key)
+                                        .Except(table.Columns
+                                                     .Cast<DataColumn>()
+                                                     .Select(x => x.ColumnName))
+                                        .Select(x => new DataColumn(x))
+                                        .ToArray();
+                table.Columns.AddRange(headers);
+
+                var addedRows = new int[length];
+                for (int i = 0; i < length; i++)
+                    addedRows[i] = table.Rows.IndexOf(table.Rows.Add());
+
+                foreach (DataColumn col in table.Columns)
+                {
+                    object[] columnRows;
+                    if (!allEntries.TryGetValue(col.ColumnName, out columnRows))
+                        continue;
+
+                    for (int i = 0; i < addedRows.Length; i++)
+                        table.Rows[addedRows[i]][col] = columnRows[i];
+                }
+            }
+
+            return table;
         }
 
         public DataTable CreateTable(IEnumerable<IDictionary<string, object>> obj)
